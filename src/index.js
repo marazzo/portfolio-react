700fa7a2f7f8b138e5ecf37a9488b0bb23b162d5
Marianne Azzopardi <mar-azzo@Mariannes-MacBook-Pro.local>
2022-08-02 23:38:56 +0100

    create react app and set up global variables

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Header } from './components/Header';
+import { MainContainer } from './components/MainContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header/>
+    <MainContainer/>
+   
   </React.StrictMode>
 );
 
